858ea42be6eaf219448b856c1d0992c7477c3163
Paul Lewallen <paullewallencom@users.noreply.github.com>
2020-12-26 16:20:40 -0800

    init
    init

---
+++ _src/spring-web-services-master/restful-web-services/backup04-basic-create-service/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,45 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	@Autowired
+	private UserDaoService service;
+
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+	
+	//
+	// input - details of user
+	// output - CREATED & Return the created URI
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user){
+		User savedUser = service.save(user);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
